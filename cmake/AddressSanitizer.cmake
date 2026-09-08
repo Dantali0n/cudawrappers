d74f42f6f207338b1e083d968806be7d1f358948
lukken <lukken@astron.nl>
2026-09-08 09:26:37 +0200

    Set test properties for cuda leak tests

--- cmake/AddressSanitizer.cmake
+++ cmake/AddressSanitizer.cmake
@@ -9,3 +9,7 @@ target_link_options(asan INTERFACE -fsanitize=address)
 function(enable_target_address_sanitizer target)
     target_link_libraries(${target} PUBLIC asan)
 endfunction()
+
+SET(ADDRESS_SANITIZER_ASAN_ENV  "ASAN_OPTIONS=" CACHE INTERNAL "ADDRESS_SANITIZER_ASAN_ENV")
+SET(ADDRESS_SANITIZER_LEAK_ENV_OPTS  "detect_leaks=0" CACHE INTERNAL "ADDRESS_SANITIZER_LEAK_ENV_OPTS")
+SET(ADDRESS_SANITIZER_CUDA_ENV_OPTS  "protect_shadow_gap=0:replace_intrin=0:detect_stack_use_after_return=0" CACHE INTERNAL "ADDRESS_SANITIZER_CUDA_ENV_OPTS")
